7538328c228db59ba3080db56215b7314e0c3f0b
Tony Ho <ho.tonym@gmail.com>
2018-05-16 18:48:38 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,21 @@
 class Person
-attr_reader :name
+attr_reader :name :happiness
 attr_accessor :bank_account
-attr_reader
+attr_writer
 
   def initialize(name)
     @name = name
     @bank_account = 25
+    @happiness = 8
   end
 
+  def happiness=(integer)
+    if integer > 10
+      integer = 10
+    end
+    @happiness = integer
+
+  end
 
 
 end
