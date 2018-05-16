086399019d9f0fb38b2f34654393792efb8ee598
Tony Ho <ho.tonym@gmail.com>
2018-05-16 18:46:14 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,13 @@
 class Person
 attr_reader :name
+attr_accessor :bank_account
+attr_reader
 
   def initialize(name)
     @name = name
+    @bank_account = 25
   end
 
-
-  def bank_account
-
-
-  end
 
 
 end
